e7b313968bc3a4aac4b2806c0d39835ecaa078cf
bchaoqun <bchaoqun@qq.com>
2023-09-08 16:39:43 +0800

    增加cuda支持

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,15 @@
 
 #include "Preliminaries.h"
 
+using namespace std;
+
 int main() {
-    Preliminaries::Preliminaries::ndarray();
+    //    Preliminaries::Preliminaries::ndarray();
+    torch::Tensor output;
+    cout << "cuda is_available: " << torch::cuda::is_available() << endl;
+    torch::DeviceType device = at::kCPU;
+    if (torch::cuda::is_available()) device = at::kCUDA;
+    output = torch::randn({3, 3}).to(device);
+    cout << output << endl;
     return 0;
 }
